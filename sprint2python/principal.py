c6b0a02f916417f1448bb26d50069092c2eebce5
Angel Andrade <aandradeb@fpcoruna.afundacion.org>
2022-10-13 13:12:14 +0200

    Show elapsed time when calling factorial functions (#6)

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,3 +1,5 @@
+import time
+
 from factorial import func_factorial
 from factorial2 import func_factorial2
 
@@ -16,9 +18,18 @@ def main():
 
     if menu_option.lower() == "a":
         number_input = int(input("Dime un número para calcular su factorial CON recursividad: "))
+        start_time = time.time()
         print(f"El factorial de {number_input}: " + str(func_factorial(number_input)))
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
     elif menu_option.lower() == "b":
         number_input = int(input("Dime un número para calcular su factorial SIN recursividad: "))
+        start_time = time.time()
         print(f"El factorial de {number_input}: " + str(func_factorial2(number_input)))
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 
-main()
+if __name__ == '__main__':
+    main()
