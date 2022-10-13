92da436c814330064cbf460646b960c66d2e92f8
Angel Andrade <aandradeb@fpcoruna.afundacion.org>
2022-10-13 12:58:02 +0200

    Create menu to choose between functions (#5)
    Se ejecuta mas rapido el que no utiliza recursividad porque cuanto mayor sea el numero mas memoria necesita el pc y hay veces que el programa crashea

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,4 +1,24 @@
 from factorial import func_factorial
+from factorial2 import func_factorial2
 
-number_input = int(input("Dime un número para calcular su factorial: "))
-print(f"El factorial de {number_input}: " + str(func_factorial(number_input)))
+
+def display_menu():
+    """Display the menu"""
+    print("  [ Menu ]\n")
+    print("    a) Usar func_factorial CON recursividad")
+    print("    b) Usar func_factorial2 SIN recursividad")
+
+
+def main():
+    """ Main function to execute all code """
+    display_menu()
+    menu_option = input("\nEscoge una opcion: ")
+
+    if menu_option.lower() == "a":
+        number_input = int(input("Dime un número para calcular su factorial CON recursividad: "))
+        print(f"El factorial de {number_input}: " + str(func_factorial(number_input)))
+    elif menu_option.lower() == "b":
+        number_input = int(input("Dime un número para calcular su factorial SIN recursividad: "))
+        print(f"El factorial de {number_input}: " + str(func_factorial2(number_input)))
+
+main()
